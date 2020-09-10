d54fdc082be9895e675a32a4881ccb269d55f154
codr7 <codr7@outlook.com>
2020-09-10 15:22:59 +0200

    simplified example

---
+++ src/codr7/logot/Example.java
@@ -0,0 +1,23 @@
+package codr7.logot;
+
+import java.awt.*;
+
+public class Example implements Room {
+    public Example(Shape shape, Point startPosition) {
+        this.shape = shape;
+        this.startPosition = startPosition;
+    }
+
+    @Override
+    public Point getStartPosition() {
+        return startPosition;
+    }
+
+    @Override
+    public boolean contains(Point position) {
+        return shape.contains(position.x, position.y);
+    }
+
+    private Shape shape;
+    private Point startPosition;
+}
--- src/codr7/logot/Main.java
+++ src/codr7/logot/Main.java
@@ -1,11 +1,16 @@
 package codr7.logot;
 
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
 
 public class Main {
     public static void main(String[] args) {
-        new Robot(Language.SE, new Example1()).execute("HGHGGHGHG");
-        new Robot(Language.EN, new Example2()).execute("RRFLFFLRF");
+        Example ex1 = new Example(new Rectangle(5, 5), new Point(1, 2));
+        new Robot(Language.SE, ex1).execute("HGHGGHGHG");
+
+        Example ex2 = new Example(new Ellipse2D.Float(-10, -10, 20, 20), new Point(0, 0));
+        new Robot(Language.EN, ex2).execute("RRFLFFLRF");
+        
         // TODO add tests
     }
 }
